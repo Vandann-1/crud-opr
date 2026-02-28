53e3c946918a838efa9ddfa893a5b89659bdf1ea
Vandann-1 <vandanparmar6041@gmail.com>
2026-02-28 22:06:57 +0530

    DD

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ofv/', views.orderFormView, name='order_url'),
+    path('form/', views.orderFormView, name='order_url'),
     path('', views.showView, name='show_url'),
-    path('up/<int:f_oid>', views.updateView, name= 'update_url'),
-    path('del/<int:f_oid>', views.deleteView, name= 'delete_url'),
+    path('update/<int:f_oid>', views.updateView, name= 'update_url'),
+    path('delete/<int:f_oid>', views.deleteView, name= 'delete_url'),
 ]
